e296dc3340df5d12a10d1275d777bc5e91eb82e7
defiant00 <defiant00@gmail.com>
2023-12-15 22:04:40 -0800

    command line args and versions

--- src/build/main.zig
+++ src/build/main.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
-const Color = @import("shared").Color;
+const Allocator = std.mem.Allocator;
+const shared = @import("shared");
 
 pub const lib = @cImport({
     @cInclude("SDL.h");
 });
+
+const version = std.SemanticVersion{ .major = 0, .minor = 1, .patch = 0 };
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -12,57 +15,91 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(alloc);
     defer std.process.argsFree(alloc, args);
 
-    if (args.len == 2) {
-        std.debug.print("Converting {s}\n", .{args[1]});
-
-        const img = lib.SDL_LoadBMP(args[1]) orelse {
-            lib.SDL_Log("Unable to load image: %s", lib.SDL_GetError());
-            return error.SDLError;
-        };
-
-        const path_parts = [_][]const u8{ args[1], ".g16" };
-        const out_path = try std.mem.concat(alloc, u8, &path_parts);
-        const out_file = try std.fs.cwd().createFile(out_path, .{});
-        defer out_file.close();
-
-        var comp = try std.compress.deflate.compressor(
-            alloc,
-            out_file.writer(),
-            .{ .level = .best_compression },
-        );
-        defer comp.deinit();
-
-        if (lib.SDL_LockSurface(img) != 0) {
-            lib.SDL_Log("Unable to lock surface: %s", lib.SDL_GetError());
-            return error.SDLError;
-        }
-
-        var writer = comp.writer();
-        try writer.writeInt(u16, @intCast(img.*.w), .little);
-        try writer.writeInt(u16, @intCast(img.*.h), .little);
-
-        if (@divTrunc(img.*.pitch, img.*.w) != 4) {
-            lib.SDL_Log("Input file must be 32 bit color.");
-            return error.InvalidInputFormat;
-        }
-
-        const count: usize = @intCast(img.*.w * img.*.h);
-        const pixels: [*]u32 = @ptrCast(@alignCast(img.*.pixels));
-        var r: u8 = 0;
-        var g: u8 = 0;
-        var b: u8 = 0;
-        var a: u8 = 0;
-        for (0..count) |i| {
-            lib.SDL_GetRGBA(pixels[i], img.*.format, &r, &g, &b, &a);
-            const uc = Color.to5551(r, g, b, a);
-            try writer.writeInt(u16, uc, .little);
-        }
-
-        try comp.close();
-
-        lib.SDL_UnlockSurface(img);
-        lib.SDL_FreeSurface(img);
-
-        std.debug.print("  Done\n", .{});
+    if (args.len == 2 and std.ascii.eqlIgnoreCase(args[1], "help")) {
+        printUsage();
+    } else if (args.len == 2 and std.ascii.eqlIgnoreCase(args[1], "version")) {
+        std.debug.print(
+            \\release {}
+            \\  build {}
+            \\
+        , .{
+            shared.release,
+            version,
+        });
+    } else if (args.len == 2 and std.ascii.endsWithIgnoreCase(args[1], ".bmp")) {
+        try convertBitmap(alloc, args[1]);
+    } else if (args.len > 1) {
+        // todo - build
+    } else {
+        printUsage();
+        return error.InvalidCommand;
     }
+}
+
+fn convertBitmap(alloc: Allocator, path: [:0]const u8) !void {
+    std.debug.print("Converting {s}\n", .{path});
+
+    const img = lib.SDL_LoadBMP(path) orelse {
+        lib.SDL_Log("Unable to load image: %s", lib.SDL_GetError());
+        return error.SDLError;
+    };
+
+    const path_parts = [_][]const u8{ path, ".g16" };
+    const out_path = try std.mem.concat(alloc, u8, &path_parts);
+    defer alloc.free(out_path);
+    const out_file = try std.fs.cwd().createFile(out_path, .{});
+    defer out_file.close();
+
+    var comp = try std.compress.deflate.compressor(
+        alloc,
+        out_file.writer(),
+        .{ .level = .best_compression },
+    );
+    defer comp.deinit();
+
+    if (lib.SDL_LockSurface(img) != 0) {
+        lib.SDL_Log("Unable to lock surface: %s", lib.SDL_GetError());
+        return error.SDLError;
+    }
+
+    var writer = comp.writer();
+    try writer.writeInt(u16, @intCast(img.*.w), .little);
+    try writer.writeInt(u16, @intCast(img.*.h), .little);
+
+    if (@divTrunc(img.*.pitch, img.*.w) != 4) {
+        lib.SDL_Log("Input file must be 32 bit color.");
+        return error.InvalidInputFormat;
+    }
+
+    const count: usize = @intCast(img.*.w * img.*.h);
+    const pixels: [*]u32 = @ptrCast(@alignCast(img.*.pixels));
+    var r: u8 = 0;
+    var g: u8 = 0;
+    var b: u8 = 0;
+    var a: u8 = 0;
+    for (0..count) |i| {
+        lib.SDL_GetRGBA(pixels[i], img.*.format, &r, &g, &b, &a);
+        const uc = shared.Color.to5551(r, g, b, a);
+        try writer.writeInt(u16, uc, .little);
+    }
+
+    try comp.close();
+
+    lib.SDL_UnlockSurface(img);
+    lib.SDL_FreeSurface(img);
+
+    std.debug.print("  Done\n", .{});
+}
+
+fn printUsage() void {
+    std.debug.print(
+        \\Usage: build [command]
+        \\
+        \\Commands:
+        \\  [files]    Build specified files
+        \\
+        \\  help       Print this help and exit
+        \\  version    Print versions and exit
+        \\
+    , .{});
 }
--- src/play/main.zig
+++ src/play/main.zig
@@ -1,10 +1,44 @@
 const std = @import("std");
-const Color = @import("shared").Color;
+const Allocator = std.mem.Allocator;
+const shared = @import("shared");
+const Color = shared.Color;
 const sdl = @import("sdl.zig");
+
+const version = std.SemanticVersion{ .major = 0, .minor = 1, .patch = 0 };
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
+
+    const args = try std.process.argsAlloc(alloc);
+    defer std.process.argsFree(alloc, args);
+
+    if (args.len == 1) {
+        try play(alloc, null);
+    } else if (args.len == 2) {
+        if (std.ascii.eqlIgnoreCase(args[1], "help")) {
+            printUsage();
+        } else if (std.ascii.eqlIgnoreCase(args[1], "version")) {
+            std.debug.print(
+                \\release {}
+                \\   play {}
+                \\
+            , .{
+                shared.release,
+                version,
+            });
+        } else {
+            try play(alloc, args[1]);
+        }
+    } else {
+        printUsage();
+        return error.InvalidCommand;
+    }
+}
+
+fn play(alloc: Allocator, path: ?[:0]const u8) !void {
+    // todo - load from path if not null
+    _ = path;
 
     var sdl_inst = try sdl.init(alloc);
     defer sdl_inst.deinit();
@@ -52,4 +86,17 @@ pub fn main() !void {
         try sdl_inst.renderFramebuffer();
         sdl_inst.present();
     }
+}
+
+fn printUsage() void {
+    std.debug.print(
+        \\Usage: play [command]
+        \\
+        \\Commands:
+        \\  [file]     Play specified file
+        \\
+        \\  help       Print this help and exit
+        \\  version    Print versions and exit
+        \\
+    , .{});
 }
