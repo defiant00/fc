28e7f060b870c3b49aaa4ed38cec2049e622eed8
defiant00 <defiant00@gmail.com>
2023-12-16 23:27:11 -0800

    rename

--- src/shared/shared.zig
+++ src/shared/shared.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub const Color = @import("color.zig");
+pub const Color = @import("Color.zig");
 pub const graphics = @embedFile("graphics.g16");
 
 pub const release = std.SemanticVersion{ .major = 2023, .minor = 12, .patch = 1 };
